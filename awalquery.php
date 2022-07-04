a8e216d1bf4be333dfca2b7d2563d6fb0f4e8472
primadwi84 <69444257+primadwi84@users.noreply.github.com>
2022-07-04 21:55:29 +0800

    Delete awalquery.php

--- awalquery.php
+++
@@ -1,8 +0,0 @@
-<html>
-<title>Form Upload</title>
-<body>
-<form enctype="multipart/form-data" method="POST" action="querytf2.php">
-Kata Kunci : <br>
-  <input type="text" name="keyword"><br>
-<input type=submit value=Submit>
-</form>
