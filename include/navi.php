71267f5e0afdf64c0721b7a4ee576754d7576d33
dwiprima01 <dwiprimahandayaniputri@gmail.com>
2022-07-04 21:44:13 +0800

    first commit

---
+++ include/navi.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Data Mining</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+      <link href="css/style.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="upload.php">Upload Dokumen</a></li>
+        <li><a href="querytf2.php">Bobot</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'lib/Enhanced_CS.php';
+include('include/navi.php');
+?>
+
+<div class="jumbotron text-center">
+  <h1>DATA MINING</h1>
+  <h2>Tokenisasi - Filtering - Stemming - Bobot</h2>
+</div>
+
+<div class="container pt">
+        <div class="row mt">
+            <div class="col-lg-6 col-lg-offset-3 centered">
+                <h3> Nama Kelompok </h3>
+            </div>
+  <br>
+  <br>
+  <hr>
+                <p align="center">
+                Ketut Gede Subiksha (1915101007) <br>
+                Ni Luh Putu Risma Dewi (1915101008) <br>
+                Gusti Ayu Gita Mulya Sari (1915101011) <br>
+                Dwi Prima Handayani Putri (1915101012) <br>
+
+             </div>
+           </div>
+
+
